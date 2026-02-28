13aaa34cc81f34b1b14dbc94bb9914f2419ffddb
Lexingtoner <horrorpain99@gmail.com>
2026-02-28 16:42:08 +1000

    Fix rendering error, update SDK and add face detection UI improvements

--- app/src/main/java/java/com/pdm/ml_face_detection/presentation/UIState.kt
+++ app/src/main/java/java/com/pdm/ml_face_detection/presentation/UIState.kt
@@ -5,5 +5,6 @@ import com.pdm.ml_face_detection.domain.models.FaceResult
 
 data class UIState(
     val camera: Int = CameraSelector.LENS_FACING_BACK,
-    val faceResult: FaceResult = FaceResult()
+    val faceResult: FaceResult = FaceResult(),
+    val requirementsMeet: Boolean = false
 )
